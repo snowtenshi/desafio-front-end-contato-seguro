61486d2efd10961d90227afcaba8be28bd81af4f
Gabriela Mariano <gabrielamariano@live.com>
2025-02-03 20:47:55 -0300

    feat: adiciona botões de voltar para home nas telas de listas, ajusta e cria classes para controlar melhor o layout de visualização das páginas

--- desafio-crud-livros/src/pages/AuthorsListPage.tsx
+++ desafio-crud-livros/src/pages/AuthorsListPage.tsx
@@ -1,17 +1,25 @@
 import { useState } from "react";
 import AuthorFormModal from "../components/AuthorFormModal";
+import '../assets/css/list-page.css'
+import { Link } from "react-router-dom";
 
 function BooksListPage() {
 
   const [isAuthorModalOpen, setIsAuthorModalOpen] = useState(false);
 
   return (
-    <section>
-      <h1>Lista de Autores</h1>
-    
-      <div>
-        <button className='home-button' onClick={() => setIsAuthorModalOpen(true)}>Adicionar Autores</button>
+    <section className="list-container">
+      <div className="list-container__title">
+        <h1>Lista de Autores</h1>
       </div>
+    
+      <div className="button-container">
+        <button className='home-button' onClick={() => setIsAuthorModalOpen(true)}>Adicionar Autores</button>
+        <Link to='/'>
+          <button>Voltar</button>
+        </Link>
+      </div>
+
 
       {isAuthorModalOpen && (
         <AuthorFormModal onClose={() => setIsAuthorModalOpen(false)} />
--- desafio-crud-livros/src/pages/BooksListPage.tsx
+++ desafio-crud-livros/src/pages/BooksListPage.tsx
@@ -1,16 +1,24 @@
 import { useState } from "react";
 import BookFormModal from "../components/BookFormModal";
+import '../assets/css/list-page.css';
+import '../assets/css/global.css';
+import { Link } from "react-router-dom";
 
 function BooksListPage() {
 
   const [isBookModalOpen, setIsBookModalOpen] = useState(false);
 
   return (
-    <section>
-      <h1>Lista de Livros</h1>
+    <section className="list-container">
+      <div className="list-container__title">
+        <h1>Lista de Livros</h1>
+      </div>
     
-      <div>
+      <div className='button-container'>
         <button className='home-button' onClick={() => setIsBookModalOpen(true)}>Adicionar Livros</button>
+        <Link to='/'>
+          <button>Voltar</button>
+        </Link>
       </div>
 
       {isBookModalOpen && (

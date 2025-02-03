a90cad28e422f9de0f695d7377f3493ac700e1e5
Gabriela Mariano <gabrielamariano@live.com>
2025-02-03 19:05:01 -0300

    feat: adiciona botões para os modais na home e cria o formulário de modal para os livros

---
+++ desafio-crud-livros/src/components/BookFormModal.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "../assets/css/modal.css";
+
+interface ModalProps {
+  onClose: () => void;
+}
+
+const BookFormModal: React.FC<ModalProps> = ({ onClose }) => {
+  return (
+    <div className="modal-container">
+      <div className="modal-content">
+        <form>
+          <label>
+            Nome do Livro: <input type="text" required />
+          </label>
+
+          <label>
+            ID do Autor: <input type="text" required />
+          </label>
+
+          <label>
+            Número de Páginas: &#40;Opcional&#41; <input type="number" />
+          </label>
+
+          <button type="submit">Salvar</button>
+        </form>
+
+        <button className="close-button" onClick={onClose}>
+          Fechar
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default BookFormModal;
--- desafio-crud-livros/src/pages/Home.tsx
+++ desafio-crud-livros/src/pages/Home.tsx
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../assets/css/home.css';
 import { Link } from 'react-router-dom';
+import BookFormModal from '../components/BookFormModal';
+import AuthorFormModal from '../components/AuthorFormModal';
 
 function Home() {
+
+  const [isBookModalOpen, setIsBookModalOpen] = useState(false);
+  const [isAuthorModalOpen, setIsAuthorModalOpen] = useState(false);
+
   return (
     <main className='home-container'>
       <div className='home-container__title'>
         <h1>Seja bem-vindo&#40;a&#41; Biblioteca Pública da Contato Seguro</h1>
         <p>v0.1</p>
       </div>
+
       <div className='button-container'>
         <Link to="/books">
           <button className='home-button'>Lista de Livros</button>
-        </Link>
+        </Link>        
         <Link to="/authors">
           <button className='home-button'>Lista de Autores</button>
         </Link>
+
+        <button className='home-button' onClick={() => setIsBookModalOpen(true)}>Adicionar Livros</button>
+
+        <button className='home-button' onClick={() => setIsAuthorModalOpen(true)}>Adicionar Autores</button>
       </div>
+
+      {isBookModalOpen && (
+        <BookFormModal onClose={() => setIsBookModalOpen(false)} />
+      )}
+
+      {isAuthorModalOpen && (
+        <AuthorFormModal onClose={() => setIsAuthorModalOpen(false)} />
+      )}
+
     </main>
   );
 }

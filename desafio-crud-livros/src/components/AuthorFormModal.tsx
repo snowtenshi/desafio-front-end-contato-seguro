7fa5d4c9438ff16949100b68894b519c2685596e
Gabriela Mariano <gabrielamariano@live.com>
2025-02-04 14:03:39 -0300

    feat: adiciona função para salvar dados do formulário no localStorage

--- desafio-crud-livros/src/components/AuthorFormModal.tsx
+++ desafio-crud-livros/src/components/AuthorFormModal.tsx
@@ -1,33 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import { Author } from "../types";
 import "../assets/css/modal.css";
-import "../assets/css/form.css"
+import "../assets/css/form.css";
 
 interface ModalProps {
   onClose: () => void;
 }
 
 const AuthorFormModal: React.FC<ModalProps> = ({ onClose }) => {
+  const [name, setName] = useState("");
+  const [authorId, setAuthorId] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const newAuthor: Author = {
+      id: authorId,
+      name: name,
+      email: email,
+    };
+
+    const storedAuthors = localStorage.getItem("authors");
+    const authors: Author[] = storedAuthors ? JSON.parse(storedAuthors) : [];
+
+    authors.push(newAuthor);
+    localStorage.setItem("authors", JSON.stringify(authors));
+
+    setName("");
+    setAuthorId("");
+    setEmail("");
+    onClose();
+  };
+
   return (
     <div className="modal-container">
       <div className="modal-content">
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>
-            Nome do Autor: <input type="text" required />
+            Nome do Autor:
+            <input
+              type="text"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+              required
+            />
           </label>
 
           <label>
-            ID do Autor: <input type="text" required />
+            ID do Autor:
+            <input
+              type="text"
+              value={authorId}
+              onChange={(event) => setAuthorId(event.target.value)}
+              required
+            />
           </label>
 
           <label>
-            Email do Autor: &#40;Opcional&#41; <input type="text" />
+            Email do Autor: &#40;Opcional&#41;
+            <input
+              type="text"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
           </label>
+          <div className="button-container">
+            <button className="save-button" type="submit">
+              Salvar
+            </button>
+            <button className="close-button" onClick={onClose}>
+              Fechar
+            </button>
+          </div>
         </form>
-        
-        <div className="button-container">
-          <button className="save-button" type="submit">Salvar</button>
-          <button className="close-button" onClick={onClose}>Fechar</button>
-        </div>
       </div>
     </div>
   );
--- desafio-crud-livros/src/components/BookFormModal.tsx
+++ desafio-crud-livros/src/components/BookFormModal.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Book } from "../types";
 import "../assets/css/modal.css";
 import "../assets/css/form.css";
 
@@ -7,31 +8,73 @@ interface ModalProps {
 }
 
 const BookFormModal: React.FC<ModalProps> = ({ onClose }) => {
+  const [name, setName] = useState("");
+  const [authorId, setAuthorId] = useState("");
+  const [pages, setPages] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const newBook: Book = {
+      id: Date.now().toString(),
+      name: name,
+      authorId: String(authorId),
+      pages: String(pages),
+    };
+
+    const storedBooks = localStorage.getItem("books");
+    const books: Book[] = storedBooks ? JSON.parse(storedBooks) : [];
+
+    books.push(newBook);
+    localStorage.setItem("books", JSON.stringify(books));
+
+    setName("");
+    setAuthorId("");
+    setPages("");
+    onClose();
+  };
+
   return (
     <div className="modal-container">
       <div className="modal-content">
-        <form>
-        <label>
-            ID do Livro: <input type="text" required />
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome do Livro:
+            <input
+              type="text"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+              required
+            />
           </label>
 
           <label>
-            Nome do Livro: <input type="text" required />
+            ID do Autor:
+            <input
+              type="text"
+              value={authorId}
+              onChange={(event) => setAuthorId(event.target.value)}
+              required
+            />
           </label>
 
           <label>
-            ID do Autor: <input type="text" required />
+            Número de Páginas &#40;Opcional&#41;:
+            <input
+              type="text"
+              value={pages}
+              onChange={(event) => setPages(event.target.value)}
+            />
           </label>
-
-          <label>
-            Número de Páginas &#40;Opcional&#41;: <input type="text" />
-          </label>
+          <div className="button-container">
+            <button className="save-button" type="submit">
+              Salvar
+            </button>
+            <button className="close-button" onClick={onClose}>
+              Fechar
+            </button>
+          </div>
         </form>
-
-        <div className="button-container">
-          <button className="save-button" type="submit">Salvar</button>
-          <button className="close-button" onClick={onClose}>Fechar</button>
-        </div>
       </div>
     </div>
   );
